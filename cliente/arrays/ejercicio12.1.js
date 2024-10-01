fd89b5f448a95deb7aab9bffc697a83d8b8cbf28
davidp209 <131154233+davidp209@users.noreply.github.com>
2024-10-01 23:59:00 +0200

    01/10

---
+++ cliente/arrays/ejercicio12.1.js
@@ -0,0 +1,18 @@
+window.onload=function(){
+
+    const tVector = [2,3,5,6,8,7];
+   let x = prompt("Introduce un numero");
+   function numIgual(x)
+   {
+       for( i =0; i < tVector.length; i ++ )
+   
+       if(tVector[i] == x){
+           res = i;
+           
+       }else {
+           res = -1;
+           
+       }}
+   console.log(numeros.find(numIgual));
+   
+   }
